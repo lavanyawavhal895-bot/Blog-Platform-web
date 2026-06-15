44dbfda3619642c75bd0d28aaf81dc65019d26c9
Lavanya <lavanyawavhal895@users.noreply.github.com>
2026-06-15 18:49:58 +0530

    debug routing

--- Client/src/routes/AppRoutes.tsx
+++ Client/src/routes/AppRoutes.tsx
@@ -3,42 +3,60 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import ForgotPassword from "../pages/ForgotPassword";
+import ResetPassword from "../pages/ResetPassword";
+
 import Dashboard from "../pages/Dashboard";
 import AdminDashboard from "../pages/AdminDashboard";
 import CreateBlog from "../pages/CreateBlog";
 import MyBlogs from "../pages/MyBlogs";
 import BlogDetails from "../pages/BlogDetails";
 import EditBlog from "../pages/EditBlog";
-import ResetPassword from "../pages/ResetPassword";
+import Profile from "../pages/Profile"; // NEW
 
 import ProtectedRoute from "../components/ProtectedRoute";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      {/* Public */}
+      {/* Public Routes */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password/:token" element={<ResetPassword />} />
-      {/* User Routes */}
+      <Route
+        path="/reset-password/:token"
+        element={<ResetPassword />}
+      />
+
+      {/* Protected User Routes */}
       <Route element={<ProtectedRoute />}>
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route
+          path="/"
+          element={<Navigate to="/dashboard" replace />}
+        />
 
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} /> {/* NEW */}
         <Route path="/create-blog" element={<CreateBlog />} />
         <Route path="/my-blogs" element={<MyBlogs />} />
         <Route path="/blog/:id" element={<BlogDetails />} />
         <Route path="/edit-blog/:id" element={<EditBlog />} />
       </Route>
 
-      {/* Admin */}
+      {/* Admin Routes */}
       <Route element={<ProtectedRoute requiredRole="admin" />}>
         <Route path="/admin" element={<AdminDashboard />} />
       </Route>
 
       {/* 404 */}
-      <Route path="*" element={<Navigate to="/dashboard" replace />} />
+   {/* 404 */}
+          <Route
+            path="*"
+            element={
+              <div className="min-h-screen flex items-center justify-center text-red-500 text-4xl">
+                404 ROUTE HIT
+              </div>
+            }
+          />
     </Routes>
   );
 };
