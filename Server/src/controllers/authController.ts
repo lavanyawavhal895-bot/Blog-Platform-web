16992e7df6adc9dc2a29503568925e2b94344a40
Lavanya <lavanyawavhal895@users.noreply.github.com>
2026-06-15 14:37:42 +0530

    added user already exist

--- Server/src/controllers/authController.ts
+++ Server/src/controllers/authController.ts
@@ -19,10 +19,21 @@ export const register = async (req: Request, res: Response) => {
   try {
     const { username, email, password } = req.body;
 
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res.status(400).json({ message: "User already exists" });
-    }
+    const emailExists = await User.findOne({ email });
+
+          if (emailExists) {
+            return res.status(400).json({
+              message: "Email already exists",
+            });
+          }
+
+          const usernameExists = await User.findOne({ username });
+
+          if (usernameExists) {
+            return res.status(400).json({
+              message: "Username already exists",
+            });
+          }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const otp = Math.floor(100000 + Math.random() * 900000).toString();
