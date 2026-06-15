f90dbf1d10313d4c31dd23d9b256ef5874a4fa6c
Lavanya <lavanyawavhal895@users.noreply.github.com>
2026-06-15 18:54:21 +0530

    test profile page

--- Client/src/pages/Profile.tsx
+++ Client/src/pages/Profile.tsx
@@ -1,21 +1,25 @@
 import { useAuth } from "../context/AuthContext";
 
 const Profile = () => {
-  const { user, token, loading } = useAuth();
+  const { user } = useAuth();
 
   return (
     <div className="min-h-screen p-10 text-white">
-      <pre>
-        {JSON.stringify(
-          {
-            loading,
-            token,
-            user,
-          },
-          null,
-          2
-        )}
-      </pre>
+      <h1 className="text-4xl font-bold mb-6">My Profile</h1>
+
+      <div className="bg-slate-900 p-6 rounded-xl max-w-md">
+        <p>
+          <strong>Username:</strong> {user?.username}
+        </p>
+
+        <p>
+          <strong>Email:</strong> {user?.email}
+        </p>
+
+        <p>
+          <strong>Role:</strong> {user?.role}
+        </p>
+      </div>
     </div>
   );
 };
